d308eb20a39a39abb31fe6b6dac4bcaf8dfda557
Geogueddari <abdeljabbarelgaddari24@gmail.com>
2025-04-14 17:33:57 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+import DashboardLayoutBasic  from "./components/DashboardLayoutBasic"
+
+function App() {
+
+  return (
+    <DashboardLayoutBasic/>
+  )
+}
+
+export default App
---
+++ src/components/Map.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from 'react';
+
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import { fromLonLat } from 'ol/proj';
+import TileLayer from 'ol/layer/Tile.js';
+import OSM from 'ol/source/OSM.js';
+import 'ol/ol.css';
+
+export default function MapComponent() {
+    const mapRef = useRef(null); // pour le DOM
+    const olMapRef = useRef(null); // pour stocker l'objet Map
+
+    useEffect(() => {
+        if (!olMapRef.current) {
+            olMapRef.current = new Map({
+                target: mapRef.current,
+                layers: [
+                    new TileLayer({
+                        source: new OSM(),
+                    }),
+                ],
+                view: new View({
+                    center: fromLonLat([-7.589843, 33.573110]), // coordonnées de casablanca
+                    zoom: 11,
+                }),
+            });
+        }
+    }, []);
+
+    return (
+        <div
+            ref={mapRef}
+            style={{
+                width: '100%',
+                height: '90%',
+                borderRadius: "10px",
+                overflow: "hidden",
+                boxShadow: "0 10px 20px rgba(16, 16, 16, 0.1)",
+            }}
+        />
+
+    );
+}
